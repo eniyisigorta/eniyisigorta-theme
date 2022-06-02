91a7ee1b4a37da5d8df7e3b429ea0c5ef9f2603e
imdecoder <coderwax@gmail.com>
2022-06-02 17:38:22 +0300

    Dosyalar güncellendi.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
 $(function () {
 
+	$('.hero-area').css({
+		height: $(window).height() - ($('.global-nav').height() + $('.local-nav').height())
+	});
+
+	$(window).resize(function () {
+		$('.hero-area').css({
+			height: $(window).height() - ($('.global-nav').height() + $('.local-nav').height())
+		});
+	});
+
+	$('.hero-arrow').click(function () {
+		$('html, body').animate({
+			scrollTop: $('.local-nav').height() + $('.hero-area').height()
+		}, 'slow');
+	});
+
 })
